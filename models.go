c1e460f775a9a7a73ba421161fd27076596e10fb
Paweł Kowalak <pawel.kowalak@gmail.com>
2016-03-18 23:24:04 +0100

    Add more coverage reports structs

--- models.go
+++ models.go
@@ -1,11 +1,11 @@
 package qfarm
 
 import (
-	"time"
 	"encoding/json"
 	"fmt"
-"strings"
 	"regexp"
+	"strings"
+	"time"
 )
 
 // Build represents single build.
@@ -57,30 +57,51 @@ type CoverageReport struct {
 
 // PackageReport holds info about coverage analysis of specified package.
 type PackageReport struct {
-	Name     string        `json:"name"`
-	Coverage float64       `json:"coverage"`
-	Failed   bool          `json:"failed"`
-	Html     string        `json:"html"`
-	TestsNo  int           `json:"testsNo"`
-	PassedNo int           `json:"passedNo"`
-	FailedNo int           `json:"failedNo"`
-	Time     time.Duration `json:"time"`
+	Name     string            `json:"name"`
+	Coverage float64           `json:"coverage"`
+	Failed   bool              `json:"failed"`
+	TestsNo  int               `json:"testsNo"`
+	PassedNo int               `json:"passedNo"`
+	FailedNo int               `json:"failedNo"`
+	Time     time.Duration     `json:"time"`
+	Files    []CoverFileReport `json:"files"`
+}
+
+// CoverFileReport holds coverage report for single file.
+type CoverFileReport struct {
+	Name     string `json:"name"`
+	Coverage float64
+	Blocks   []CoverBlock
+}
+
+// CoverBlock holds part of coverage profile for single block in file.
+type CoverBlock struct {
+	Start   Cursor `json:"start"`
+	End     Cursor `json:"end"`
+	NumStmt int    `json:"numStmt"`
+	Count   int    `json:"count"`
+}
+
+// Cursor points at specific location in file and is used by CoverBlock.
+type Cursor struct {
+	Line int `json:"line"`
+	Col  int `json:"col"`
 }
 
 // Node represents a node in directory tree. It might be a file or a directory.
 type Node struct {
-	Path     string  `json:"path"`
-	Nodes    []*Node  `json:"nodes"`
-	Parent   *Node   `json:"parent"`
-	Dir      bool    `json:"dir"`
-	Coverage float64 `json:"coverage"`
-	TestsNo  int     `json:"testsNo"`
-	FailedNo int     `json:"failedNo"`
-	PassedNo int     `json:"passedNo"`
-	IssuesNo int     `json:"issuesNo"`
-	ErrorsNo int     `json:"errorsNo"`
-	WarningsNo int   `json:"warningsNo"`
-	Issues   []*Issue `json:"issues"`
+	Path       string   `json:"path"`
+	Nodes      []*Node  `json:"nodes"`
+	Parent     *Node    `json:"parent"`
+	Dir        bool     `json:"dir"`
+	Coverage   float64  `json:"coverage"`
+	TestsNo    int      `json:"testsNo"`
+	FailedNo   int      `json:"failedNo"`
+	PassedNo   int      `json:"passedNo"`
+	IssuesNo   int      `json:"issuesNo"`
+	ErrorsNo   int      `json:"errorsNo"`
+	WarningsNo int      `json:"warningsNo"`
+	Issues     []*Issue `json:"issues"`
 }
 
 // Linter represents linter details. It's used in metalinter.
@@ -94,7 +115,7 @@ type Linter struct {
 	MessageOverride  string   `json:"message_override,omitempty"`
 	EventType        string
 
-	Regex            *regexp.Regexp
+	Regex *regexp.Regexp
 }
 
 // MarshalJSON marshals struct to JSON.
@@ -134,4 +155,3 @@ func (i *Issue) String() string {
 	}
 	return fmt.Sprintf("%s:%d:%s:%s: %s (%s)", strings.TrimSpace(i.Path), i.Line, col, i.Severity, strings.TrimSpace(i.Message), i.Linter)
 }
-
