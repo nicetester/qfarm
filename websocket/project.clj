1da3ff017c4e2b93b8d3882db8fdc355321993f3
Konrad Mrozek <konrad.mrozek@gmail.com>
2016-03-18 17:53:38 +0100

    Websocket service initial commit

---
+++ websocket/project.clj
@@ -0,0 +1,13 @@
+(defproject qfarm-websocket "0.1.0-SNAPSHOT"
+  :description "quality.farm websocket service"
+  :url "https://github.com/qfarm/qfarm"
+  :license {:name "The MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [aleph "0.4.1-beta5"]
+                 [mount "0.1.10"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [com.taoensso/carmine "2.12.2"]]
+  :main ^:skip-aot qfarm-websocket.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
