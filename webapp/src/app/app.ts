723673e9a858a6fb62bfdf41cc887a22176010c1
Mateusz Dymiński <dyminski@gmail.com>
2016-03-19 15:17:41 +0100

    Add confguration page

--- webapp/src/app/app.ts
+++ webapp/src/app/app.ts
@@ -4,6 +4,7 @@ import {WebSocketService} from './services/websocket.service';
 
 import { Entry } from './entry/entry';
 import { Build } from './builds/build';
+import { Config } from './config/config';
 
 @Component({
     selector: 'app',
@@ -18,6 +19,7 @@ import { Build } from './builds/build';
     { path: '/build/:repoName/', component: Build, name: 'Last Build' },
     { path: '/build/:repoName/:buildId/', component: Build, name: 'Build' },
     { path: '/build/:repoName/:buildId/:file/', component: Build, name: 'Build - File View' }
+    { path: '/config', component: Config, name: 'Config - Get QFarm Configuration' }
 ])
 export class App {
     name = 'Quality Farm';
